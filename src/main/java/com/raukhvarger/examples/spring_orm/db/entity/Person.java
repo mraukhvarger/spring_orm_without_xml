d38e9a7b99be30f709683fbcc87fb4443b8b0275
Maxim Raukhvarger <raukhvarger@gmail.com>
2016-11-05 14:08:23 +0300

    Убрал геттеры и сеттеры у Персон, для примера они не нужны

--- src/main/java/com/raukhvarger/examples/spring_orm/db/entity/Person.java
+++ src/main/java/com/raukhvarger/examples/spring_orm/db/entity/Person.java
@@ -13,11 +13,11 @@ public class Person {
 
     @Id
     @GeneratedValue
-    Long id;
+    public Long id;
 
-    String name;
+    public String name;
 
-    Integer age;
+    public Integer age;
 
     @Override
     public String toString() {
@@ -35,27 +35,4 @@ public class Person {
         this.age = age;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
